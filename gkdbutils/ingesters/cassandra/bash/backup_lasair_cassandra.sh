3fadac06eb52b09c21d041f797a9ee5cef30b905
Ken Smith <k.w.smith@qub.ac.uk>
2024-02-15 11:29:26 +0000

    Added forcedphot to backup_lasair_cassandra.sh

--- gkdbutils/ingesters/cassandra/bash/backup_lasair_cassandra.sh
+++ gkdbutils/ingesters/cassandra/bash/backup_lasair_cassandra.sh
@@ -16,8 +16,13 @@ export REMOTE_LOCATION=$3
 
 export CURRENT_BACKUP_CANDIDATES=$DIRECTORY/backup_candidates.csv
 export PREVIOUS_BACKUP_CANDIDATES=$DIRECTORY/backup_candidates.old.csv
+
 export CURRENT_BACKUP_NONCANDIDATES=$DIRECTORY/backup_noncandidates.csv
 export PREVIOUS_BACKUP_NONCANDIDATES=$DIRECTORY/backup_noncandidates.old.csv
+
+export CURRENT_BACKUP_FORCEDPHOT=$DIRECTORY/backup_forcedphot.csv
+export PREVIOUS_BACKUP_FORCEDPHOT=$DIRECTORY/backup_forcedphot.old.csv
+
 export DAY=`date +"%a"`
 
 cp -p $CURRENT_BACKUP_CANDIDATES $PREVIOUS_BACKUP_CANDIDATES
@@ -31,6 +36,15 @@ echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE completed
 sleep 1
 cp -p $CURRENT_BACKUP_NONCANDIDATES $PREVIOUS_BACKUP_NONCANDIDATES
 
+export TABLE=forcedphot
+export FILE=$CURRENT_BACKUP_NONCANDIDATES
+echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE started
+/usr/bin/cqlsh -e "COPY $KEYSPACE.$TABLE TO '$FILE' WITH HEADER = TRUE;"
+echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE completed
+
+sleep 1
+cp -p $CURRENT_BACKUP_FORCEDPHOT $PREVIOUS_BACKUP_FORCEDPHOT
+
 export TABLE=noncandidates
 export FILE=$CURRENT_BACKUP_NONCANDIDATES
 echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE started
@@ -39,7 +53,10 @@ echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE completed
 
 rm $CURRENT_BACKUP_CANDIDATES.gz
 rm $CURRENT_BACKUP_NONCANDIDATES.gz
+rm $CURRENT_BACKUP_FORCEDPHOT.gz
 pigz $CURRENT_BACKUP_CANDIDATES
 pigz $CURRENT_BACKUP_NONCANDIDATES
+pigz $CURRENT_BACKUP_FORCEDPHOT
 scp -p $CURRENT_BACKUP_CANDIDATES.gz $REMOTE_LOCATION/candidates.$DAY.gz
 scp -p $CURRENT_BACKUP_NONCANDIDATES.gz $REMOTE_LOCATION/noncandidates.$DAY.gz
+scp -p $CURRENT_BACKUP_FORCEDPHOT.gz $REMOTE_LOCATION/forcedphot.$DAY.gz
