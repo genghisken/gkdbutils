ce67682d995ab5570d64b74269a97c3e13cac4bd
Ken Smith <k.w.smith@qub.ac.uk>
2024-03-01 10:34:41 +0000

    Reorganised backup_lasair_cassandra.sh so that uncompressed data does not take up unnecessary space when doing the backup.

--- gkdbutils/ingesters/cassandra/bash/backup_lasair_cassandra.sh
+++ gkdbutils/ingesters/cassandra/bash/backup_lasair_cassandra.sh
@@ -25,7 +25,10 @@ export PREVIOUS_BACKUP_FORCEDPHOT=$DIRECTORY/backup_forcedphot.old.csv
 
 export DAY=`date +"%a"`
 
-cp -p $CURRENT_BACKUP_CANDIDATES $PREVIOUS_BACKUP_CANDIDATES
+# *************** CANDIDATES *****************
+
+cp -p $CURRENT_BACKUP_CANDIDATES.gz $PREVIOUS_BACKUP_CANDIDATES.gz
+rm $CURRENT_BACKUP_CANDIDATES.gz
 
 export TABLE=candidates
 export FILE=$CURRENT_BACKUP_CANDIDATES
@@ -34,7 +37,14 @@ echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE started
 echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE completed
 
 sleep 1
-cp -p $CURRENT_BACKUP_NONCANDIDATES $PREVIOUS_BACKUP_NONCANDIDATES
+
+pigz $CURRENT_BACKUP_CANDIDATES
+scp -p $CURRENT_BACKUP_CANDIDATES.gz $REMOTE_LOCATION/candidates.$DAY.gz
+
+# *************** FORCEDPHOT *****************
+
+cp -p $CURRENT_BACKUP_FORCEDPHOT.gz $PREVIOUS_BACKUP_FORCEDPHOT.gz
+rm $CURRENT_BACKUP_FORCEDPHOT.gz
 
 export TABLE=forcedphot
 export FILE=$CURRENT_BACKUP_NONCANDIDATES
@@ -43,7 +53,15 @@ echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE started
 echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE completed
 
 sleep 1
-cp -p $CURRENT_BACKUP_FORCEDPHOT $PREVIOUS_BACKUP_FORCEDPHOT
+
+pigz $CURRENT_BACKUP_FORCEDPHOT
+scp -p $CURRENT_BACKUP_FORCEDPHOT.gz $REMOTE_LOCATION/forcedphot.$DAY.gz
+
+
+# ************** NONCANDIDATES ***************
+
+cp -p $CURRENT_BACKUP_NONCANDIDATES.gz $PREVIOUS_BACKUP_NONCANDIDATES.gz
+rm $CURRENT_BACKUP_NONCANDIDATES.gz
 
 export TABLE=noncandidates
 export FILE=$CURRENT_BACKUP_NONCANDIDATES
@@ -51,12 +69,9 @@ echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE started
 /usr/bin/cqlsh -e "COPY $KEYSPACE.$TABLE TO '$FILE' WITH HEADER = TRUE;"
 echo `date '+%Y-%m-%dT%H:%M:%S'` Backup of $TABLE completed
 
-rm $CURRENT_BACKUP_CANDIDATES.gz
-rm $CURRENT_BACKUP_NONCANDIDATES.gz
-rm $CURRENT_BACKUP_FORCEDPHOT.gz
-pigz $CURRENT_BACKUP_CANDIDATES
+sleep 1
+
 pigz $CURRENT_BACKUP_NONCANDIDATES
-pigz $CURRENT_BACKUP_FORCEDPHOT
-scp -p $CURRENT_BACKUP_CANDIDATES.gz $REMOTE_LOCATION/candidates.$DAY.gz
 scp -p $CURRENT_BACKUP_NONCANDIDATES.gz $REMOTE_LOCATION/noncandidates.$DAY.gz
-scp -p $CURRENT_BACKUP_FORCEDPHOT.gz $REMOTE_LOCATION/forcedphot.$DAY.gz
+
+
